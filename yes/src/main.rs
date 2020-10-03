27bb81250be74eb222289302f5ab5b8bcabb3131
Sean Breckenridge <seanbrecke@gmail.com>
2020-10-03 04:21:24 -0700

    remove unused Result annotation

--- yes/src/main.rs
+++ yes/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::io::{self, Write};
 
-fn main() -> io::Result<()> {
+fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
     let mut buf: &str = &args.join(" ");
     if buf.is_empty() {
